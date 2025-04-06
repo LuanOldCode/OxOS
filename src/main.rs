90927506e798fefdc2d8329c0c29fcd52143f4e6
luanoldcode <luangotardodeveloper@gmail.com>
2025-04-05 22:00:45 -0300

    Alfa - V0.0.001

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+    loop {}
+}
